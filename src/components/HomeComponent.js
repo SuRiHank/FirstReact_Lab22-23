65382501e3b84fc2e241a351b38bc9e7f1fa9906
Hank <dangphuochan2003@gmail.com>
2023-10-18 17:36:39 +0700

    Uncontrolled Form

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -25,7 +25,7 @@ function Home(props) {
                     <RenderCard item={props.dish} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.dish} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
