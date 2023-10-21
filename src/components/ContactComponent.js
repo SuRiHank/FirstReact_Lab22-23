df488777e2ee382a66f35c24bdd0975129084bae
Hank <dangphuochan2003@gmail.com>
2023-10-21 14:15:17 +0700

    React Redux Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Col,
-  Row,
-  FormFeedback,
+  Breadcrumb,BreadcrumbItem,Button,Form,FormGroup,Label,Input,Col,Row, FormFeedback,
 } from "reactstrap";
-
 import { Link } from "react-router-dom";
-
 import { Control, LocalForm, Errors } from "react-redux-form";
 
 class Contact extends Component {
